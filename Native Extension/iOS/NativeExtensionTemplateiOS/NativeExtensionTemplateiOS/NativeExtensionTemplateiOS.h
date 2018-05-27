264de366278c545f98cc3bedac9297f3a9b667c3
Johan Degraeve <johan.degraeve@gmail.com>
2018-05-27 20:38:20 +0200

    Compile with AIR 29 with Native Extension - was missing in previous commit - Native Extension is now part of the project - ant script to build the Native Extension - Native Extension has only one function, which is to do NSLog

--- Native Extension/iOS/NativeExtensionTemplateiOS/NativeExtensionTemplateiOS/NativeExtensionTemplateiOS.h
+++ Native Extension/iOS/NativeExtensionTemplateiOS/NativeExtensionTemplateiOS/NativeExtensionTemplateiOS.h
@@ -1,13 +1,6 @@
-//
-//  NativeExtensionTemplateiOS.h
-//  NativeExtensionTemplateiOS
-//
-//  Created by Johan Degraeve on 24/05/2018.
-//  Copyright © 2018 Johan Degraeve. All rights reserved.
-//
+#import "FlashRuntimeExtensions.h"
 
-#ifndef NativeExtensionTemplateiOS_h
-#define NativeExtensionTemplateiOS_h
-
-
-#endif /* NativeExtensionTemplateiOS_h */
+void NativeExtensionTemplateContextInitializer(void* extData, const uint8_t* ctxType, FREContext ctx, uint32_t* numFunctionsToTest, const FRENamedFunction** functionsToSet);
+void NativeExtensionTemplateContextFinalizer(FREContext ctx);
+void NativeExtensionTemplateExtensionInitializer(void** extDataToSet, FREContextInitializer* ctxInitializerToSet, FREContextFinalizer* ctxFinalizerToSet);
+void NativeExtensionTemplateExtensionFinalizer(void *extData);
